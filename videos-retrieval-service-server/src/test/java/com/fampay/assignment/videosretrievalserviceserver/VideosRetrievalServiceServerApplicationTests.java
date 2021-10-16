b838287af15b2560831428f294ba272d2527e33c
Saumay Khandelwal <khandelwalsaumay@gmail.com>
2021-10-16 05:34:24 +0530

    Submodules added for server and persistence

---
+++ videos-retrieval-service-server/src/test/java/com/fampay/assignment/videosretrievalserviceserver/VideosRetrievalServiceServerApplicationTests.java
@@ -0,0 +1,11 @@
+package com.fampay.assignment.videosretrievalserviceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class VideosRetrievalServiceServerApplicationTests {
+
+    @Test void contextLoads() {
+    }
+
+}
