c59dc92bb6d95109814ed01bbe3602cca3a01a5b
Saumay Khandelwal <khandelwalsaumay@gmail.com>
2021-10-18 05:28:58 +0530

    Fetching all videos for testing

--- videos-retrieval-service-server/src/main/java/com/fampay/assignment/videosretrievalserviceserver/service/VideosRetrievalService.java
+++ videos-retrieval-service-server/src/main/java/com/fampay/assignment/videosretrievalserviceserver/service/VideosRetrievalService.java
@@ -1,9 +1,14 @@
 package com.fampay.assignment.videosretrievalserviceserver.service;
 
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
+import com.fampay.assignment.videosretrievalserviceserver.db.VideoEntity;
+import com.fampay.assignment.videosretrievalserviceserver.db.VideoRepository;
 
 @Service
 public class VideosRetrievalService {
@@ -11,8 +16,15 @@ public class VideosRetrievalService {
     @Autowired
     private VideosRetrievalWorker videosRetrievalWorker;
 
+    @Autowired
+    private VideoRepository videoRepository;
+
     @PostConstruct
     public void runWorker() {
         videosRetrievalWorker.callYoutubeDataApi();
+    }
+
+    public List<VideoEntity> getAllEntities() {
+        return videoRepository.findAll();
     }
 }
