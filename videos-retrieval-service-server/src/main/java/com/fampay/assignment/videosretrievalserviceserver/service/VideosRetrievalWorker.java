d83f1f5eaba533320325f989976f31d726095e32
Saumay Khandelwal <khandelwalsaumay@gmail.com>
2021-10-18 05:32:25 +0530

    Saving response retrieved from youtube api

--- videos-retrieval-service-server/src/main/java/com/fampay/assignment/videosretrievalserviceserver/service/VideosRetrievalWorker.java
+++ videos-retrieval-service-server/src/main/java/com/fampay/assignment/videosretrievalserviceserver/service/VideosRetrievalWorker.java
@@ -1,20 +1,29 @@
 package com.fampay.assignment.videosretrievalserviceserver.service;
 
-import com.fampay.assignment.videosretrievalserviceserver.utils.ConversionUtils;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import lombok.extern.slf4j.Slf4j;
+import com.fampay.assignment.videosretrievalserviceserver.db.VideoEntity;
+import com.fampay.assignment.videosretrievalserviceserver.db.VideoRepository;
+import com.fampay.assignment.videosretrievalserviceserver.utils.ConversionUtils;
+import com.fampay.assignment.videosretrievalserviceserver.utils.DateTimeUtils;
 
-import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
 
 @Component
 @Slf4j
 public class VideosRetrievalWorker {
 
-    @Value("${youtube.data.api.frequency.period.millis}")
+    @Value("${youtube.search.api.frequency.period.millis}")
     private long delay;
+
+    @Autowired
+    private VideoRepository videoRepository;
 
     @Autowired
     private WebClientService webClientService;
@@ -22,15 +31,39 @@ public class VideosRetrievalWorker {
     @Autowired
     private ConversionUtils conversionUtils;
 
+    @Autowired
+    private DateTimeUtils dateTimeUtils;
+
     public void callYoutubeDataApi() {
 
         new java.util.Timer().schedule(new java.util.TimerTask() {
 
             @Override
             public void run() {
-                Map<String, String> response = webClientService.sendGetRequest();
+                Map response = webClientService.sendGetRequest();
                 log.info("Response: {}", conversionUtils.pojoToString(response));
+
+                videoRepository.saveAll(getVideoEntityList(response));
             }
         }, 0, delay);
+    }
+
+    private List<VideoEntity> getVideoEntityList(Map<String, Object> response) {
+        List<Map> items = (List) response.get("items");
+        return items.stream().map(this::buildVideoEntity)
+                .collect(Collectors.toList());
+    }
+
+    private VideoEntity buildVideoEntity(Map item) {
+        VideoEntity videoEntity = new VideoEntity();
+        Map<String, String> idMap = (Map) item.get("id");
+        videoEntity.setId(idMap.get("videoId"));
+
+        Map snippetMap = (Map) item.get("snippet");
+        videoEntity.setTitle((String) snippetMap.get("title"));
+        videoEntity.setDescription((String) snippetMap.get("description"));
+
+        videoEntity.setPublishedAt((dateTimeUtils.convertIsoDateToDate((String) snippetMap.get("publishedAt"))));
+        return videoEntity;
     }
 }
