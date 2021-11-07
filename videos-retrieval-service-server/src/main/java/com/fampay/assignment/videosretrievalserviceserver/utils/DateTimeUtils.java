0a0b0b3eed0487c460caabab81184d62b5b456ab
Saumay Khandelwal <khandelwalsaumay@gmail.com>
2021-11-07 22:41:15 +0530

    Method renamed and unused methods commented

--- videos-retrieval-service-server/src/main/java/com/fampay/assignment/videosretrievalserviceserver/utils/DateTimeUtils.java
+++ videos-retrieval-service-server/src/main/java/com/fampay/assignment/videosretrievalserviceserver/utils/DateTimeUtils.java
@@ -12,7 +12,20 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class DateTimeUtils {
 
-    public Date convertIsoDateToDate(String isoDate) {
+    public Date convertIsoDateStrToDate(String isoDate) {
         return DatatypeConverter.parseDateTime(isoDate).getTime();
     }
+
+//    public Optional<String> convertDateToIsoDateStr(Date date) {
+////        DateTimeFormatter formatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
+//        if(Objects.nonNull(date)) {
+//            String dateStr = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").format(date);
+//            return Optional.of(dateStr);
+//        }
+//        return Optional.empty();
+//    }
+//
+//    public Date convertMillisToDate(long millis) {
+//        return new Date(millis);
+//    }
 }
