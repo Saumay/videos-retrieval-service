35b54d7c8c1b7dd38628221bfbfdd411d4e963c8
Saumay Khandelwal <khandelwalsaumay@gmail.com>
2021-11-07 22:39:31 +0530

    Enabled pagination for dashboard. Added logic for fetching next API page upon hitting the last page

--- videos-retrieval-service-server/src/main/java/com/fampay/assignment/videosretrievalserviceserver/service/VideosRetrievalService.java
+++ videos-retrieval-service-server/src/main/java/com/fampay/assignment/videosretrievalserviceserver/service/VideosRetrievalService.java
@@ -5,10 +5,14 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import com.fampay.assignment.videosretrievalserviceserver.db.VideoEntity;
 import com.fampay.assignment.videosretrievalserviceserver.db.VideoRepository;
+import com.fampay.assignment.videosretrievalserviceserver.paging.Paged;
+import com.fampay.assignment.videosretrievalserviceserver.paging.Paging;
 
 @Service
 public class VideosRetrievalService {
@@ -21,7 +25,17 @@ public class VideosRetrievalService {
 
     @PostConstruct
     public void runWorker() {
-        videosRetrievalWorker.callYoutubeDataApi();
+        videosRetrievalWorker.schedule();
+    }
+
+    public Paged<VideoEntity> getPage(int pageNumber, int size) {
+        PageRequest request = PageRequest.of(pageNumber - 1, size);
+        Page<VideoEntity> postPage = videoRepository.findAll(request);
+        if(pageNumber > postPage.getTotalPages()) {
+            videosRetrievalWorker.getNextApiPageAndSaveApiResults();
+            postPage = videoRepository.findAll(request);
+        }
+        return new Paged<>(postPage, Paging.of(postPage.getTotalPages(), pageNumber, size));
     }
 
     public List<VideoEntity> getAllEntities() {
