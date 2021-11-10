da86928a049eae45e5dc1c44f9c37f0326d88921
Saumay Khandelwal <v-saumay.khandelwal@rksv.in>
2021-11-10 13:36:08 +0530

    Method added to Repository for fetching latestUpdatedDate

--- src/main/java/com/fampay/assignment/videosretrievalserviceserver/db/VideoRepository.java
+++ src/main/java/com/fampay/assignment/videosretrievalserviceserver/db/VideoRepository.java
@@ -7,8 +7,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+
 @Repository
 public interface VideoRepository extends JpaRepository<VideoEntity, Long> {
+
+  @Query("SELECT (MAX(v.publishedAt)) FROM VideoEntity v")
+  Date getLatestPublishedDate();
 
   @Query(value = "SELECT ve FROM VideoEntity ve WHERE UPPER(ve.title) LIKE %:searchQuery% OR UPPER(ve.description) LIKE %:searchQuery% OR UPPER(ve.channel) LIKE %:searchQuery%")
   Page<VideoEntity> findAllBySearchQuery(@Param("searchQuery") String searchQuery, Pageable pageable);
